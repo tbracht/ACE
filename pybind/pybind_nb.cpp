27d999d00c5314448387f0dbf8609b51a23aeac0
mcygorek <mo_cygorek@gmx.de>
2026-07-14 23:50:42 +0200

    propagate_Taylor_threshold

--- pybind/pybind_nb.cpp
+++ pybind/pybind_nb.cpp
@@ -125,7 +125,7 @@ NB_MODULE(_ACE, m) {
          &ACE::FreePropagator::update,
          nb::call_guard<nb::gil_scoped_release>())
     .def_rw("propagate_Taylor",           &ACE::FreePropagator::propagate_Taylor)
-    .def_rw("propagate_system_threshold", &ACE::FreePropagator::propagate_system_threshold)
+    .def_rw("propagate_Taylor_threshold", &ACE::FreePropagator::propagate_Taylor_threshold)
     .def_rw("const_H",                    &ACE::FreePropagator::const_H)
     .def_rw("M",                          &ACE::FreePropagator::M)
     ;
